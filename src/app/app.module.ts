9cab1061bab55f6b0653cf0cb8f74f4d6b5ef63a
Bilel AIT ALLA <bilel.aitalla66@gmail.com>
2023-03-15 10:56:30 +0100

    ajout des routes et le composant pokemon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { PokemonsComponent } from './pokemons/pokemons.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'pokemons', component: PokemonsComponent }
+];
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatIconModule],
+  declarations: [AppComponent, HeaderComponent, PokemonsComponent],
+  imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatIconModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
