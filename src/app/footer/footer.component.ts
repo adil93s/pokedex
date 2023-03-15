228224edeb9dc64a8ee9507d7e4d0f8a33e1430b
adil93s <adil.chet@gmail.com>
2023-03-15 12:55:33 +0100

    changement du style du footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,11 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent {
-  onLinkClick(link: string) {
-    console.log(`Link clicked: ${link}`);
-  }
-}
-
+export class FooterComponent {}
