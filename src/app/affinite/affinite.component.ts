a7173756a0a708c8f791095f369ac06cbf796dfe
mghlouci <merwan.ghlouci@gmail.com>
2023-03-15 16:45:48 +0100

    recup donnees API

--- src/app/affinite/affinite.component.ts
+++ src/app/affinite/affinite.component.ts
@@ -7,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AffiniteComponent implements OnInit {
   title = 'HTTP using native fetch API';
-  private url: string = 'https://pokebuildapi.fr/api/v2/type';
+  private url: string = 'https://pokeapi.co/api/v2/type';
 
   isLoading: boolean = true;
   data: any;
+  types : any;
 
   ngOnInit(): void {
     fetch(this.url)
@@ -19,5 +20,14 @@ export class AffiniteComponent implements OnInit {
         this.isLoading = false;
         this.data = AffiniteData;
       });
+
+    for(let i=1;i<20;i++){
+      fetch('https://pokeapi.co/api/v2/type/'+i)
+      .then((response) => response.json())
+      .then((typesdata) => {
+        this.isLoading = false;
+        this.types = typesdata;
+      });
+    }
   }
 }
