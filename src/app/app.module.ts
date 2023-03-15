443cc4cf6e14c46990e64c231978c6701520ff97
ismailSKR <ismailgada94@gmail.com>
2023-03-15 12:38:28 +0100

    Ajout favoris avec visualisation pokemon fiche descriptive et statistique

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,16 @@ import { MatIconModule } from '@angular/material/icon';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PokemonsComponent } from './pokemons/pokemons.component';
+import { FavorisComponent } from './favoris/favoris.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'pokemons', component: PokemonsComponent }
+  { path: 'pokemons', component: PokemonsComponent },
+  { path: 'favorites', component: FavorisComponent },
 ];
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, PokemonsComponent, PokemonsComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, PokemonsComponent, PokemonsComponent, FavorisComponent],
   imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatIconModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
