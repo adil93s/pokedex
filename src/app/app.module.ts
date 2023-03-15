7617a2f1dfcf84e62a7b0f8d4c37c886e599ef7c
Bilel AIT ALLA <bilel.aitalla66@gmail.com>
2023-03-15 11:03:54 +0100

    ajout du home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { PokemonsComponent } from './pokemons/pokemons.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: HomeComponent },
   { path: 'pokemons', component: PokemonsComponent }
 ];
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, PokemonsComponent],
+  declarations: [AppComponent, HeaderComponent, PokemonsComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatIconModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
