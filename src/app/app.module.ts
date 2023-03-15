8d9bc9b7e269949b8fc9f3f6a08aa96e2447dbd0
Bilel AIT ALLA <bilel.aitalla66@gmail.com>
2023-03-15 11:07:02 +0100

    Ajout des routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,18 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-/* import { PokemonsComponent } from './pokemons/pokemons.component'; */
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PokemonsComponent } from './pokemons/pokemons.component';
 
-/* const appRoutes: Routes = [
-  { path: '', component: AppComponent },
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'pokemons', component: PokemonsComponent }
-]; */
+];
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, /* PokemonsComponent */],
-  imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatIconModule, /* RouterModule.forRoot(appRoutes) */],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, PokemonsComponent, PokemonsComponent],
+  imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatIconModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
