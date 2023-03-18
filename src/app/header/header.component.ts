891c38d6e5e280d1089e50cf1c63eb3c7d81525f
adil93s <adil.chet@gmail.com>
2023-03-18 18:22:02 +0100

    changement du style de la page pokemons, ajout d'une animation fixed pour le header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  navbarVisible = true;
+  previousScrollPosition = window.pageYOffset;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScrollPos = window.pageYOffset;
+    if (currentScrollPos >= 1) {
+      this.navbarVisible = false;
+    } else {
+      this.navbarVisible = true;
+    }
+    if (currentScrollPos < this.previousScrollPosition) {
+      this.navbarVisible = true;
+    }
+    this.previousScrollPosition = currentScrollPos;
+  }
+}
